2aad5cd6042cdc510829adb929fde990c21f0f24
inshikchoo-hue <inshikchoo@gmail.com>
2025-12-17 19:38:06 +0900

    뉴스 리스트, 뷰 클래스명 수정

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,9 +1,9 @@
 $(function(){
-    $(".news__tab--menu li").on("click", function(){
+    $(".news__tab-menu li").on("click", function(){
         let idx = $(this).index();
 
-        $(".news__tab--menu li").removeClass("on");
-        $(this).addClass("on");
+        $(".news__tab-menu li").removeClass("news__tab-menu--active");
+        $(this).addClass("news__tab-menu--active");
 
         $(".news__contents > div").hide();
         $(".news__contents > div").eq(idx).show();
@@ -12,10 +12,10 @@ $(function(){
 })
 
 $(function(){
-    $(".pagination__btn--num").on("click", function(){
+    $(".pagination__btn-num").on("click", function(){
 
-        $(".pagination__btn--num").removeClass("on");
-        $(this).addClass("on");
+        $(".pagination__btn-num").removeClass("pagination__btn-num--active");
+        $(this).addClass("pagination__btn-num--active");
 
     });
 })
