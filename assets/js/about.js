fdaa59012896672240856f2956cc080e0604be41
janghye1019 <jhw1317@gmail.com>
2025-12-23 12:19:21 +0900

    new_list 패딩수정 및 a태그 수정, about_지도 마커 수정

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,30 +1,23 @@
 // --------------about-map----------------
-$(function() {
+$(function () {
     // 지도를 표시할 div
-    let $mapContainer = $('#map')[0]; 
-    
+    let $mapContainer = $('#map')[0];
+
     let mapOption = {
         center: new kakao.maps.LatLng(37.450356, 126.702780), // 지도의 중심좌표
         level: 3 // 지도의 확대 레벨
     };
 
-    // 지도를 생성합니다
+    // // 지도를 생성합니다
     let map = new kakao.maps.Map($mapContainer, mapOption);
+    let markerPosition = new kakao.maps.LatLng(37.450356, 126.702780);
 
-    // 마커 이미지 설정
-    let imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', 
-        imageSize = new kakao.maps.Size(64, 69), 
-        imageOption = { offset: new kakao.maps.Point(27, 69) };
-
-    let markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-        markerPosition = new kakao.maps.LatLng(37.450356, 126.702780);
-
-    // 마커 생성
+    // // 마커 생성
     let marker = new kakao.maps.Marker({
-        position: markerPosition,
-        image: markerImage
+        position: markerPosition
     });
 
-    // 지도에 마커 표시
+    // // 지도에 마커 표시
     marker.setMap(map);
+
 });
