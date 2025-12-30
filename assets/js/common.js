c8418d1b311fbc88e3c83f28060b99b51c489f5d
janghye1019 <jhw1317@gmail.com>
2025-12-30 17:22:21 +0900

    헤더링크연결 및 모바일 헤더 수정

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,15 +1,15 @@
 AOS.init({
-    duration: 950, 
+    duration: 950,
     easing: 'ease-in-out'
 });
 
 
 // GNB 기능
-$(".header__hamburger").on("click", function() {
+$(".header__hamburger").on("click", function () {
     $(".side-menu").stop().slideDown();
 });
 
-$(".side-menu").on("click", function() {
+$(".side-menu").on("click", function () {
     $(".side-menu").stop().slideUp();
 })
 
